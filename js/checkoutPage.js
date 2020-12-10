41160542a4bca64fc16168f1cde77406062d88a0
Lovisa Bohman <lovisabo@hotmail.com>
2020-12-10 14:19:09 +0100

    Continue on checkoutpage

---
+++ js/checkoutPage.js
@@ -0,0 +1,13 @@
+$(function () {
+    checkoutRender();
+});
+
+function checkoutRender() {
+    cartContainer = $(".cartContainer");
+    $("<h2>").html("Checkout").appendTo
+
+    $("<div>").addClass("productContainer");
+
+    $("<hr>");
+
+}
