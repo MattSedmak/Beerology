f11480ef2695286831cf5d09c7533fb494e123da
Lovisa Bohman <lovisabo@hotmail.com>
2020-12-18 10:47:45 +0100

    Finnished the styling for checkoutPage, added phrase to thankYouPage, removed outcommented code

--- js/thankYouPage.js
+++ js/thankYouPage.js
@@ -6,7 +6,7 @@ function orderConfirmation() {
   let firstname = JSON.parse(sessionStorage.getItem("customerInfo")) || [];
 
   let orderNumb = Math.round(Math.random() * 100000);
-  $("<p>").html(firstname).appendTo(".orderPayedText");
+  $("<p>").html(firstname + ", your order is on the way!").appendTo(".orderPayedText");
   $("<p>")
     .html("Order number: " + orderNumb)
     .appendTo(".orderPayedText");
